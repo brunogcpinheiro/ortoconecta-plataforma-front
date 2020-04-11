8ba2fd213f6b439984fd2c015561ecf08a98a0a6
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2020-04-11 10:36:29 -0300

    fix 1104

--- src/components/header.js
+++ src/components/header.js
@@ -1,70 +1,70 @@
 import React, { Fragment, Component } from "react";
 import styled from "styled-components";
-import {Link} from "../../routes";
+import { Link } from "../../routes";
 
 const primaryColor = "#ffd32a";
 
-const Navigation = styled.div`
-	height: 80px;
-	display: flex;
-	align-items: center;
-	justify-content: space-around;
-	list-style: none;
-	background: #fff;
-	width: 100%;
-	-webkit-box-shadow: 0px 3px 5px 0px rgba(0,0,0,0, 1);
-	-moz-box-shadow: 0px 3px 5px 0px rgba(0,0,0,0, 1);
-	box-shadow: 0px 3px 5px 0px rgba(0,0,0,0, 1);
+const MenuIcon = styled.div`
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
 
-	@media (max-width: 1128px) {
-		ul {
-			display: none;
-		}
-
-		${MenuIcon} .icon {
-			display: block;
-			float: right;
-		}
-	}
+  div {
+    width: 35px;
+    height: 5px;
+    background-color: #999;
+    margin: 2px 0;
+    display: none;
+  }
 `;
 
-const MenuIcon = styled.div`
-	display: flex;
-	flex-direction: column;
-	cursor: pointer;
+const Navigation = styled.div`
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  list-style: none;
+  background: #fff;
+  width: 100%;
+  -webkit-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0, 1);
+  -moz-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0, 1);
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0, 1);
 
-	div {
-		width: 35px;
-		height: 5px;
-		background-color: #999;
-		margin: 2px 0;
-		display: none;
-	}
+  @media (max-width: 1128px) {
+    ul {
+      display: none;
+    }
+
+    ${MenuIcon} .icon {
+      display: block;
+      float: right;
+    }
+  }
 `;
 
 const Logo = styled.div`
-	background: transparent;
-	a {
-		display: flex;
-		text-decoration: none;
-		color: inherit;
-		justify-content: center;
-		align-items: center;
-		background: transparent;
+  background: transparent;
+  a {
+    display: flex;
+    text-decoration: none;
+    color: inherit;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
 
-		img {
-			width: 70px;
-			height: 70px;
-			background: transparent;
-		}
+    img {
+      width: 70px;
+      height: 70px;
+      background: transparent;
+    }
 
-		h2 {
-			font-size: 2rem;
-			margin-left: 16px;
-			color: ${primaryColor};
-			background: transparent;
-		}
-	}
+    h2 {
+      font-size: 2rem;
+      margin-left: 16px;
+      color: ${primaryColor};
+      background: transparent;
+    }
+  }
 `;
 
 const Menu = styled.ul`
@@ -88,7 +88,7 @@ const Menu = styled.ul`
 
       &:after {
         display: block;
-        content: '';
+        content: "";
         border-bottom: solid 3px ${primaryColor};
         transform: scaleX(0);
         transition: transform 250ms ease-in-out;
@@ -103,157 +103,158 @@ const Menu = styled.ul`
         transform: scaleX(1);
       }
     }
+  }
 `;
 
 const Dropdown = styled.div`
-	position: absolute;
-	display: block;
-	width: 100%;
-	margin-top: 35px;
+  position: absolute;
+  display: block;
+  width: 100%;
+  margin-top: 35px;
 `;
 
 const DropdownContent = styled.div`
-	display: block;
-	position: absolute;
-	width: 100vw;
-	background-color: #f9f9f9;
-	box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-	padding: 12px 16px;
-	z-index: 1;
+  display: block;
+  position: absolute;
+  width: 100vw;
+  background-color: #f9f9f9;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  padding: 12px 16px;
+  z-index: 1;
 
-	li {
-		padding: 16px;
-		border-bottom: 1px solid #ccc;
+  li {
+    padding: 16px;
+    border-bottom: 1px solid #ccc;
 
-		a {
-			text-decoration: none;
-			font-size: 1.2rem;
+    a {
+      text-decoration: none;
+      font-size: 1.2rem;
 
-			&:visited,
-			&:active,
-			&:link {
-				color: inherit;
-			}
+      &:visited,
+      &:active,
+      &:link {
+        color: inherit;
+      }
 
-			&:hover {
-				color: ${primaryColor};
-			}
-		}
-	}
+      &:hover {
+        color: ${primaryColor};
+      }
+    }
+  }
 `;
 
 class Header extends Component {
-	state = {
-		showMenu: false,
-	};
+  state = {
+    showMenu: false,
+  };
 
-	handleMenuOpen = e => {
-		e.preventDefault();
+  handleMenuOpen = (e) => {
+    e.preventDefault();
 
-		this.setState(state => ({
-			showMenu: !state.showMenu,
-		}));
-	};
+    this.setState((state) => ({
+      showMenu: !state.showMenu,
+    }));
+  };
 
-	render () {
-		return (
-			<Fragment>
-				<Navigation>
-					<Logo>
-						<Link route="/">
-							<a>
-								<img src="/static/logo.jpeg" />
-								<h2>OrtoConecta</h2>
-							</a>
-						</Link>
-					</Logo>
-					<MenuIcon onClick={e => this.handleMenuOpen(e)}>
-						<div className="icon" />
-						<div className="icon" />
-						<div className="icon" />
-					</MenuIcon>
-					{this.state.showMenu === true && (
-						<Dropdown data-aos="fade-down" data-aos-duration="1000">
-							<DropdownContent>
-								<Link route="/">
-									<li>
-										<a>Início</a>
-									</li>
-								</Link>
-								<Link route="/painel">
-									<li>
-										<a>Painel OrtoConecta</a>
-									</li>
-								</Link>
-								<Link route="/materiais">
-									<li>
-										<a>Materiais</a>
-									</li>
-								</Link>
-								<Link route="/galeria">
-									<li>
-										<a>Galeria</a>
-									</li>
-								</Link>
-								<Link route="/dicas">
-									<li>
-										<a>Dicas</a>
-									</li>
-								</Link>
-								<Link route="/especialidades">
-									<li>
-										<a>Especialidades</a>
-									</li>
-								</Link>
-								<Link route="/especializacao_e_cursos">
-									<li>
-										<a>Especialização e Cursos</a>
-									</li>
-								</Link>
-							</DropdownContent>
-						</Dropdown>
-					)}
-					<Menu>
-						<li>
-							<Link route="/">
-								<a>Início</a>
-							</Link>
-						</li>
-						<li>
-							<Link route="/painel">
-								<a>Painel OrtoConecta</a>
-							</Link>
-						</li>
-						<li>
-							<Link route="/materiais">
-								<a>Materiais</a>
-							</Link>
-						</li>
-						<li>
-							<Link route="/galeria">
-								<a>Galeria</a>
-							</Link>
-						</li>
-						<li>
-							<Link route="/dicas">
-								<a>Dicas</a>
-							</Link>
-						</li>
-						<li>
-							<Link route="/especialidades">
-								<a>Especialidades</a>
-							</Link>
-						</li>
-						<li>
-							<Link route="/especializacao_e_cursos">
-								<a>Especialização e Cursos</a>
-							</Link>
-						</li>
-					</Menu>
-				</Navigation>
-			</Fragment>
-		);
-	}
+  render() {
+    return (
+      <Fragment>
+        <Navigation>
+          <Logo>
+            <Link route="/">
+              <a>
+                <img src="/static/logo.jpeg" />
+                <h2>OrtoConecta</h2>
+              </a>
+            </Link>
+          </Logo>
+          <MenuIcon onClick={(e) => this.handleMenuOpen(e)}>
+            <div className="icon" />
+            <div className="icon" />
+            <div className="icon" />
+          </MenuIcon>
+          {this.state.showMenu === true && (
+            <Dropdown data-aos="fade-down" data-aos-duration="1000">
+              <DropdownContent>
+                <Link route="/">
+                  <li>
+                    <a>Início</a>
+                  </li>
+                </Link>
+                <Link route="/painel">
+                  <li>
+                    <a>Painel OrtoConecta</a>
+                  </li>
+                </Link>
+                <Link route="/materiais">
+                  <li>
+                    <a>Materiais</a>
+                  </li>
+                </Link>
+                <Link route="/galeria">
+                  <li>
+                    <a>Galeria</a>
+                  </li>
+                </Link>
+                <Link route="/dicas">
+                  <li>
+                    <a>Dicas</a>
+                  </li>
+                </Link>
+                <Link route="/especialidades">
+                  <li>
+                    <a>Especialidades</a>
+                  </li>
+                </Link>
+                <Link route="/especializacao_e_cursos">
+                  <li>
+                    <a>Especialização e Cursos</a>
+                  </li>
+                </Link>
+              </DropdownContent>
+            </Dropdown>
+          )}
+          <Menu>
+            <li>
+              <Link route="/">
+                <a>Início</a>
+              </Link>
+            </li>
+            <li>
+              <Link route="/painel">
+                <a>Painel OrtoConecta</a>
+              </Link>
+            </li>
+            <li>
+              <Link route="/materiais">
+                <a>Materiais</a>
+              </Link>
+            </li>
+            <li>
+              <Link route="/galeria">
+                <a>Galeria</a>
+              </Link>
+            </li>
+            <li>
+              <Link route="/dicas">
+                <a>Dicas</a>
+              </Link>
+            </li>
+            <li>
+              <Link route="/especialidades">
+                <a>Especialidades</a>
+              </Link>
+            </li>
+            <li>
+              <Link route="/especializacao_e_cursos">
+                <a>Especialização e Cursos</a>
+              </Link>
+            </li>
+          </Menu>
+        </Navigation>
+      </Fragment>
+    );
+  }
 }
 
 export default Header;
